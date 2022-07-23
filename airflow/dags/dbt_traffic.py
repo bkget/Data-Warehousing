500066886e083dd5c73c8791e0c6c410a7698127
Biruk Getaneh <bkgetmom@gmail.com>
2022-07-24 01:30:17 +0300

    add dbt dag

---
+++ airflow/dags/dbt_traffic.py
@@ -0,0 +1,47 @@
+from pendulum import datetime
+from datetime import timedelta
+
+from airflow import DAG
+from airflow.operators.bash_operator import BashOperator
+
+# We're hardcoding this value here for the purpose of the demo, but in a production environment this
+# would probably come from a config file and/or environment variables!
+DBT_PROJECT_DIR = "/opt/airflow/dbt"
+
+default_args = {
+    'owner': 'airflow',
+    'depends_on_past': False,
+    'email': ['bkgetmom@gmail.com'],
+    'email_on_failure': True,
+    'email_on_retry': True,
+    'retries': 1,
+    "start_date": datetime(2022, 7, 20, 2, 30, 00),
+    'retry_delay': timedelta(minutes=5),
+}    
+
+with DAG(
+    "Traffic_dbt_dag",
+    start_date=datetime(2022, 7, 17),
+    description="A sample Airflow DAG to invoke dbt runs using a BashOperator",
+    schedule_interval=None,
+    catchup=False,
+    default_args=default_args, 
+) as dag:
+    # This task loads the CSV files from dbt/data into the local postgres database for the purpose of this demo.
+    # In practice, we'd usually expect the data to have already been loaded to the database.
+    dbt_seed = BashOperator(
+        task_id="dbt_seed", 
+        bash_command=f"dbt seed --profiles-dir {DBT_PROJECT_DIR} --project-dir {DBT_PROJECT_DIR}", 
+    )
+    
+    dbt_run = BashOperator(
+        task_id="dbt_run",
+        bash_command=f"dbt run --profiles-dir {DBT_PROJECT_DIR} --project-dir {DBT_PROJECT_DIR}",
+    )
+
+    dbt_test = BashOperator(
+        task_id="dbt_test",
+        bash_command=f"dbt test --profiles-dir {DBT_PROJECT_DIR} --project-dir {DBT_PROJECT_DIR}",
+    )
+
+    dbt_seed >> dbt_run >> dbt_test
