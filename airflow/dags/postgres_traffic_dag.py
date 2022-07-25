006ba96f157911149ea1279b9041d54c4a116c50
Biruk Getaneh <bkgetmom@gmail.com>
2022-07-25 16:49:23 +0300

    update the data read

--- airflow/dags/postgres_traffic_dag.py
+++ airflow/dags/postgres_traffic_dag.py
@@ -1,4 +1,5 @@
 # Importing the necessary modules
+from asyncore import read
 from airflow.providers.postgres.operators.postgres import PostgresOperator
 from airflow.providers.postgres.hooks.postgres import PostgresHook
 from airflow.operators.python_operator import PythonOperator
@@ -6,6 +7,7 @@ from datetime import datetime as dt
 from datetime import timedelta
 from airflow import DAG 
 import pandas as pd
+from sqlalchemy import Numeric
 
 # Specifing the default_args
 default_args = {
@@ -18,44 +20,96 @@ default_args = {
     'start_date': dt(2022, 7, 18),
     'retry_delay': timedelta(minutes=5)
 }
+ 
+def split_into_chunks(arr, n):
+    return [arr[i : i + n] for i in range(0, len(arr), n)]
 
-# Reading the data from the csv file
 def read_data():
-    df = pd.read_csv('/opt/data/sample.csv')
+    data_df = pd.read_csv('/opt/airflow/data/20181024_d1_0830_0900.csv', 
+                          skiprows=1,
+                          header=None,
+                          delimiter="\n",
+                        )
+    series = data_df[0].str.split(";")
+    pd_lines = []
+
+    for line in series:
+        old_line = [item.strip() for item in line]
+        info_index = 4
+        info = old_line[:info_index]
+        remaining = old_line[info_index:-1]
+        chunks = split_into_chunks(remaining, 6)
+        for chunk in chunks:
+            record = info + chunk
+            pd_lines.append(record)
+
+    new_data_df = pd.DataFrame(
+        pd_lines,
+        columns=[
+            "track_id",
+            "type",
+            "traveled_d",
+            "avg_speed",
+            "lat",
+            "lon",
+            "speed",
+            "lon_acc",
+            "lat_acc",
+            "time",
+        ],
+    )
+    return new_data_df
+
+def data_shape():
+    df = read_data()
     return df.shape
+
 
 # Inserting the data into the our postgres table
 def insert_data(): 
     pg_hook = PostgresHook(
-    postgres_conn_id="postgres_localhost")
+    postgres_conn_id="pg_server")
     conn = pg_hook.get_sqlalchemy_engine()
-    df = pd.read_csv('/opt/data/sample.csv')
+    data_df = read_data()
 
-    df.to_sql("traffic",
+    data_df.to_sql("traffic",
         con=conn,
         if_exists="replace",
-        index=False,
+        index=True,
+        index_label="id",
+        dtype={
+            "track_id": Numeric(),
+            "traveled_d": Numeric(),
+            "avg_speed": Numeric(),
+            "lat": Numeric(),
+            "lon": Numeric(),
+            "speed": Numeric(),
+            "lon_acc": Numeric(),
+            "lat_acc": Numeric(),
+            "time": Numeric(),
+        },
     )
 
 # Dag creation
 with DAG(
     dag_id='data_to_Postgres_loader_dag',
     default_args=default_args,
-    description='Upload data from CSV to MySQL',
+    description='Upload data from CSV to Postgres',
     schedule_interval='@once',
     catchup=False
 ) as pg_dag:
  
  data_reader = PythonOperator(
     task_id="read_data", 
-    python_callable=read_data
+    python_callable=data_shape
   )
 
  table_creator = PostgresOperator(
     task_id="create_table", 
-    postgres_conn_id="postgres_localhost",
+    postgres_conn_id="pg_server",
     sql = '''
-            CREATE TABLE  IF NOT EXISTS traffic (
+        CREATE TABLE  IF NOT EXISTS traffic( 
+            id serial primary key,
             track_id numeric, 
             type text not null, 
             traveled_d double precision DEFAULT NULL,
@@ -65,8 +119,7 @@ with DAG(
             speed double precision DEFAULT NULL,    
             lon_acc double precision DEFAULT NULL, 
             lat_acc double precision DEFAULT NULL, 
-            time double precision DEFAULT NULL,
-            primary key (track_id)
+            time double precision DEFAULT NULL
         );
     '''
     )
